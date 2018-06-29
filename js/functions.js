ed5304ea0bab999ef8884a7766d1667b90324aa0
Scott Lee <achacttn@gmail.com>
2018-06-29 17:26:18 +1000

    folder structure tidied

--- js/functions.js
+++ js/functions.js
@@ -1,56 +1,145 @@
-console.log(2);
-
 var app = app || {};
 
-app.createColor = ({ r, g, b })=>{
-    const color = new THREE.Color( r, g, b );
-    return color;
-};
+app.createSpotlight = () => {
+    const spotlight = new THREE.SpotLight(0xFFFFFF);
+    spotlight.position.set(-30, 50, 30);
+    // spotlight.castShadow = true;
+    // spotlight.shadow.mapSize.width = 2048;
+    // spotlight.shadow.mapSize.height = 2048;
 
-app.createCamera = ({ pov, aspectRatio, nearView, farView, sx, sy, sz })=>{
-    const camera = new THREE.PerspectiveCamera( pov, aspectRatio, nearView, farView );
-    camera.position.set( sx, sy, sz );
-    return camera;
-};
-
-app.createOrbitControls = ( input )=>{
-    return new THREE.OrbitControls( input, app.renderer.domElement );
-};
-
-app.animate = ()=>{
-    app.stats.update();
-    app.renderer.render( app.scene, app.camera );
-    requestAnimationFrame( app.animate );
-};
-
-app.createAmbientLight = ( color )=>{
-    const ambientLight = new THREE.AmbientLight(color);
-    return ambientLight;
-};
-
-app.createSpotlight = ( color, { sx, sy, sz })=>{
-    const spotlight = new THREE.SpotLight( color );
-    spotlight.position.set( sx, sy, sz );
-    spotlight.castShadow = true;
-    spotlight.shadow.mapSize.width = 2048;
-    spotlight.shadow.mapSize.height = 2048;
     return spotlight;
 };
 
-app.createMaterial = ( color, type )=>{
-    switch(type){
-        case "MeshLambertMaterial":
-        return new THREE.MeshLambertMaterial({color});
-        break;
-    }
+
+app.createPlane = () => {
+    const planeGeometry = new THREE.PlaneGeometry(120, 60);
+    const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xCFD8DC });
+
+    // combine geometry (shape) and material (surface)
+    // into mesh, the actual 3d object
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+    plane.rotation.x = -0.5 * Math.PI;
+
+    plane.position.x = 15;
+    plane.position.y = 0;
+    plane.position.z = 0;
+
+    // plane.receiveShadow = true;
+
+    return plane;
+};
+
+
+app.createCube = ({ x, y, z, xSize, ySize, zSize, colour }) => {
+    const cubeGeometry = new THREE.BoxGeometry(xSize, ySize, zSize);
+    const cubeMaterial = new THREE.MeshLambertMaterial({
+        color: colour,
+        wireframe: false,
+    });
+    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.position.set(x, y, z);
+    // cube.castShadow = true;
+
+    cube.rotationScale = Math.random();
+
+    return cube;
 }
 
-app.createObject = ( geometry, material )=>{
-    switch(geometry.name){
-        case "plane":
-        const planeGeometry = new THREE.PlaneGeometry( geometry.dimension.x, geometry.dimension.y );
-        const plane = new THREE.Mesh( planeGeometry, material );
-        plane.rotation.x = -0.5*Math.PI;
-        return plane;
+app.createSphere = ({ sx, sy, sz, px, py, pz, bool, colorSet }) => {
+    // spheres take radius, then 2 arguments for triangles
+    var toSet = colorSet;
+    const sphereGeometry = new THREE.SphereGeometry(sx, sy, sz);
+    const sphereMaterial = new THREE.MeshLambertMaterial({
+        color: toSet,
+        wireframe: bool,
+        map: THREE.ImageUtils.loadTexture('img/earth.jpg'),
+        side: THREE.DoubleSide
+    });
+
+    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sphere.position.set(px, py, pz);
+    // sphere.castShadow = true;
+
+    return sphere;
+};
+
+app.createParticleSystem = () => {
+    const particles = new THREE.Geometry();
+    const dist = app.controls.particleDistribution
+    for (let i = 0; i < app.controls.numParticles; i++) {
+        const particle = new THREE.Vector3(
+            THREE.Math.randInt(-dist, dist),
+            100,
+            THREE.Math.randInt(-dist, dist),
+        );
+        particle.vx = 0;
+        particle.vy = 0;
+        particle.vz = 0;
+
+        particles.vertices.push(particle);
     }
+    const particleMaterial = new THREE.PointsMaterial({
+        color: 0xFFFFFF,
+        size: 8,
+        map: THREE.ImageUtils.loadTexture('img/snowflake.png'),
+        blending: THREE.NormalBlending,
+        transparent: true,
+        alphaTest: 0.5
+    });
+    const particleSystem = new THREE.Points(particles, particleMaterial);
+    return particleSystem;
+};
+
+app.animate = () => {
+
+    app.stats.update();
+    // console.log(app.step);
+
+    // app.step += app.controls.bouncingSpeed;
+    // app.sphere.position.x = 6 + 5 * (Math.cos(app.step));
+    // app.sphere.position.y = 6 + 5 * (Math.abs(Math.sin(app.step)));
+    app.sphere.rotation.y += app.controls.rotationSpeed;
+
+    app.cubes.forEach(cube => {
+        cube.rotation.x += app.controls.rotationSpeed * cube.rotationScale;
+        cube.rotation.y += app.controls.rotationSpeed * cube.rotationScale;
+    })
+
+    app.animateParticles();
+    // app.cube.rotation.x += app.controls.rotationSpeed;
+    // app.cube.rotation.y += app.controls.rotationSpeed;
+    // app.cube.rotation.z += app.controls.rotationSpeed;
+
+    app.renderer.render(app.scene, app.camera);
+
+    requestAnimationFrame(app.animate);
 }
+
+app.animateParticles = () => {
+    const particles = app.particleSystem.geometry.vertices;
+    for (let i = 0; i < particles.length; i++) {
+        const particle = particles[i];
+        // particle.y -= app.controls.bouncingSpeed;
+
+        // if( particle.y < -app.controls.particleDistribution ){
+        //     particle.y = app.controls.particleDistribution;
+        // }
+
+
+        const distSquared = (particle.x * particle.x) + (particle.y * particle.y) + (particle.z * particle.z);
+
+        if (distSquared > 2.0) {
+            const force = (1.0 / distSquared) * -0.2;
+            particle.vx += force * particle.x;
+            particle.vy += force * particle.y;
+            particle.vz += force * particle.z;
+        }
+        particle.x += particle.vx * app.controls.velocityScale;
+        particle.y += particle.vy * app.controls.velocityScale;
+        particle.z += particle.vz * app.controls.velocityScale;
+
+    }
+    // app.particleSystem.rotation.y -= app.controls.rotationSpeed;
+    app.particleSystem.geometry.verticesNeedUpdate = true;
+};
--- js/main.js
+++ js/main.js
@@ -1,94 +1,125 @@
-console.log(1);
-
 var app = app || {};
 
-app.controls = {
-    example: 0.5,
-}
+app.step = 0;
 
-app.init = ()=>{
-    
-    // gui, renderer
+app.controls = {
+    bouncingSpeed: 0.05,
+    rotationSpeed: 0.01,
+    numParticles: 50000,
+    particleDistribution: 200,
+    velocityScale: 1.0,
+};
+
+app.init = () => {
+    console.log('AAAAA');
+
     app.gui = new dat.GUI();
-    app.gui.add( app.controls, 'example', 0, 1 );
+    app.gui.add(app.controls, 'bouncingSpeed', 0, 1);
+    app.gui.add(app.controls, 'rotationSpeed', 0, 1);
+    app.gui.add(app.controls, 'velocityScale', -2, 2);
 
     app.scene = new THREE.Scene();
+
     app.width = window.innerWidth;
     app.height = window.innerHeight;
 
+    // camera takes 4 arguments: POV, aspect ratio, near render, far render
+    app.camera = new THREE.PerspectiveCamera(60, app.width / app.height, 0.1, 1000);
+    app.camera.position.set(-30, 40, 30);
+    app.camera.lookAt(app.scene.position);
+
+
+
+    // calculates drawing of objects on lighting and camera
+    // renders it to 2D image in canvas element
     app.renderer = new THREE.WebGLRenderer();
-    app.renderer.setSize( app.width, app.height );
-    app.renderer.setClearColor( 0x000000 );
-    app.renderer.shadowMap.enabled = true;
-    app.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-    //
+    app.renderer.setSize(app.width, app.height);
+    app.renderer.setClearColor(0x000000); // background
+    // app.renderer.shadowMap.enabled = true; // computationally expensive shadows disabled by default
+    // app.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-    // axes
-    app.helperAxes = new THREE.AxesHelper(60);
-    app.scene.add( app.helperAxes );
-    //
+    // app.axes = new THREE.AxesHelper(40);
+    // app.scene.add( app.axes );
 
+    // app.plane = app.createPlane();
+    // app.scene.add( app.plane );
 
+    app.cube = app.createCube({
+        x: -4, y: 40, z: 0,
+        xSize: 4, ySize: 4, zSize: 4, colour: 0xFF0000
+    });
+    app.scene.add(app.cube);
 
+    const numCubes = 10;
+    app.cubes = [];
+    for (let i = 0; i < numCubes; i++) {
+        const cube = app.createCube({
+            xSize: 4,
+            ySize: 4,
+            zSize: 4,
+            x: THREE.Math.randInt(-100, 100),
+            y: THREE.Math.randInt(-100, 100),
+            z: THREE.Math.randInt(-100, 100),
+            colour: new THREE.Color(
+                Math.random(),
+                Math.random(),
+                Math.random(),
+            )
+        });
+        app.cubes.push(cube);
+        app.scene.add(cube);
 
-    // ambient light and spotlight
-    var ambientLight1color = app.createColor({ r: 200, g: 200, b: 200 });
-    app.ambientLight1 = app.createAmbientLight(ambientLight1color);
-    app.scene.add( app.ambientLight1 );
+    }
 
-    var spotlight1color = app.createColor({ r: 250, g: 250, b: 250 });
-    var spotlight1position = { sx: -30, sy: 50, sz: 30 };
-    app.spotlight1 = app.createSpotlight( spotlight1color, spotlight1position );
-    app.scene.add( app.spotlight1 );
+    app.sphere = app.createSphere({ sx: 20, sy: 40, sz: 40, px: 0, py: 0, pz: 0, bool: false, colorSet: 0xFFFFFF });
+    app.scene.add(app.sphere);
 
-    var camera1parameters = { pov: 60, aspectRatio: app.width/app.height, nearView: 0.1, farView: 1000, sx: -30, sy: 40, sz: 30 };
-    app.camera1 = app.createCamera(camera1parameters);
-    app.mouseControls = new THREE.OrbitControls( app.camera1, app.renderer.domElement );
-    // app.camera1.lookAt(app.scene.position);
-    app.scene.add( app.camera1 );
-    //
+    app.particleSystem = app.createParticleSystem();
+    app.scene.add(app.particleSystem);
 
-    // plane
-    var plane1color = app.createColor({ r: 25, g: 25, b: 25 });
-    var plane1material = app.createMaterial( plane1color, "MeshLambertMaterial" );
-    var plane1geometry = {
-        name: "plane",
-        dimension: { x: 120, y: 60 },
-        position: { x: 15, y: 0, z: 0 }
-    };
-    app.plane1 = app.createObject( plane1geometry, plane1material )
-    app.scene.add(app.plane1);
+    // app.sphere2 = app.createSphere(6, 40, 40, 6, 20, 15, true, 0xff3333 );
+    // app.scene.add( app.sphere2 );
 
+    app.spotlight = app.createSpotlight();
+    app.scene.add(app.spotlight);
 
-    // boilerplate
-    document.getElementById('output').appendChild( app.renderer.domElement );
+    app.ambientLight = new THREE.AmbientLight(0x666666);
+    app.scene.add(app.ambientLight);
+
+    app.mouseControls = new THREE.OrbitControls(
+        app.camera, app.renderer.domElement
+    );
+
+    // add <canvas> element created by renderer to DOM
+    document.getElementById('output').appendChild(app.renderer.domElement);
 
     app.stats = app.addStats();
 
+    // perform render
+    // app.renderer.render( app.scene, app.camera );
     app.animate();
-    //
 };
 
 window.onload = app.init;
 
-app.onResize = ()=>{
+app.onResize = () => {
     app.width = window.innerWidth;
     app.height = window.innerHeight;
 
-    app.camera.aspect = app.width/app.height;
+    app.camera.aspect = app.width / app.height;
     app.camera.updateProjectionMatrix();
 
-    app.renderer.setSize( app.width, app.height );
+    app.renderer.setSize(app.width, app.height);
 };
 
-window.addEventListener( 'resize', app.onResize, false );
+window.addEventListener('resize', app.onResize, false);
 
-app.addStats = ()=>{
+app.addStats = () => {
     const stats = new Stats();
     stats.domElement.style.position = 'absolute';
     stats.domElement.style.left = '0px';
     stats.domElement.style.top = '0px';
 
-    document.getElementById('output').appendChild( stats.domElement );
+    document.getElementById('stats').appendChild(stats.domElement);
     return stats;
 }
